bfa5eae141b5032b91e6fab66a0c689e739efdc3
André Teixeira <andre-x8@hotmail.com>
2023-09-04 17:32:10 -0300

    alteracoes no projeto

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+ // productoURL: 'http://localhost:3000/producto/'
+};
--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,8 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+  // CRUD BASICO DO TUTORIAL DO YOUTUBE
+  //https://www.youtube.com/watch?v=w4agj6AiAqc&list=PL4bT56Uw3S4zIAOX1ddx8oJS6jaD43vZC&index=7
+
+  //FALTA AULA 7
