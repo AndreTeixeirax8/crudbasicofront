c61f37d3530dd1feab0537c8907ab76465e979fc
André Teixeira <andre-x8@hotmail.com>
2023-09-05 19:55:53 -0300

    adicionado o editar

--- src/app/producto/editar-producto.component.ts
+++ src/app/producto/editar-producto.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Producto } from '../models/producto';
+import { ProductoService } from '../services/producto.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-editar-producto',
@@ -6,5 +10,76 @@ import { Component } from '@angular/core';
   styleUrls: ['./editar-producto.component.css']
 })
 export class EditarProductoComponent {
+
+   producto: Producto | undefined;
+
+  constructor(
+    private productoService: ProductoService,
+    private activatedRoute: ActivatedRoute,
+    private toastr: ToastrService,
+    private router: Router
+  ) { }
+
+  ngOnInit():void {
+    const id = this.activatedRoute.snapshot.params['id'];
+    this.productoService.detail(id).subscribe(
+      data => {
+        this.producto = data;
+      },
+      err => {
+        this.toastr.error(err.error.mensaje, 'Fail', {
+          timeOut: 3000,  positionClass: 'toast-top-center',
+        });
+        this.router.navigate(['/']);
+      }
+    );
+  }
+/*
+  onUpdate(): void {
+    const id = this.activatedRoute.snapshot.params['id'];
+    this.productoService.update(id, this.producto).subscribe(
+      data => {
+        this.toastr.success('Producto Actualizado', 'OK', {
+          timeOut: 3000, positionClass: 'toast-top-center'
+        });
+        this.router.navigate(['/']);
+      },
+      err => {
+        this.toastr.error(err.error.message, 'Fail', {
+          timeOut: 3000,  positionClass: 'toast-top-center',
+        });
+        
+      }
+    );
+  }*/
+
+  onUpdate(): void {
+    const id = this.activatedRoute.snapshot.params['id'];
+    
+    if (this.producto) {
+      this.productoService.update(id, this.producto).subscribe(
+        data => {
+          this.toastr.success('Producto Actualizado', 'OK', {
+            timeOut: 3000, positionClass: 'toast-top-center'
+          });
+          this.router.navigate(['/']);
+        },
+        err => {
+          this.toastr.error(err.error.message, 'Fail', {
+            timeOut: 3000,  positionClass: 'toast-top-center',
+          });
+        }
+      );
+    }
+  }
+
+  
+  volver(): void {
+    this.router.navigate(['/']);
+  }
+
+  
+  
+  
 
 }
